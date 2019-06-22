cbfd394ee9758f0e0a624a3fbff9ca6b98358128
geofferey <geofferey.eakins@gmail.com>
2019-06-22 15:36:59 +0000

    Changes for full system-less

--- common/disable.sh
+++ common/disable.sh
@@ -13,7 +13,7 @@ HOME=/mnt
 CLOUDROOTMOUNTPOINT=$HOME/cloud/
 mkdir -p $CLOUDROOTMOUNTPOINT
 
-$MODDIR/system/bin/rclone listremotes --config ${CONFIGFILE}|cut -f1 -d: |
+$MODDIR/rclone listremotes --config ${CONFIGFILE}|cut -f1 -d: |
         while read line; do
                 echo "UNmounting... $line"
                 umount -f ${CLOUDROOTMOUNTPOINT}/${line}
--- common/service.sh
+++ common/service.sh
@@ -5,6 +5,21 @@
 # This will make sure your module will still work
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
+IMG=/sbin/.core/img
+id=rclone-mount
+
+if [ -d $IMG/$id ]; then
+
+    ln -sf $IMG/$id/rclone /sbin/rclone
+    ln -sf $IMG/$id/fusermount /sbin/fusermount
+
+else
+
+    ln -sf $MODDIR/rclone /sbin/rclone
+    ln -sf $MODDIR/fusermount /sbin/fusermount
+    
+fi
+    
 sleep 120
 echo "mounting remotes..."
 #RCLONE PARAMETERS
@@ -22,11 +37,11 @@ mkdir -p /storage/cache-backend/
 
 #sh -c "$MODDIR/system/bin/rclone mount piyushDOTgarg_shopDOTmega: ${CLOUDROOTMOUNTPOINT}/piyushDOTgarg_shopDOTmega -vv --config ${CONFIGFILE} --attr-timeout 10m --cache-dir=/storage/cache --vfs-cache-mode writes --vfs-cache-max-age 168h0m0s --log-file /sdcard/dns.log --allow-other --gid 1015" &
 
-$MODDIR/system/bin/rclone listremotes --config ${CONFIGFILE}|cut -f1 -d: |
+$MODDIR/rclone listremotes --config ${CONFIGFILE}|cut -f1 -d: |
         while read line; do
                 echo "mounting... $line"
                 mkdir -p ${CLOUDROOTMOUNTPOINT}/${line}
-                $MODDIR/system/bin/rclone mount ${line}: ${CLOUDROOTMOUNTPOINT}/${line} --config ${CONFIGFILE} --max-read-ahead ${READAHEAD} --buffer-size ${BUFFERSIZE} --dir-cache-time ${DIRCACHETIME} --poll-interval 5m --attr-timeout ${DIRCACHETIME} --vfs-cache-mode writes --vfs-read-chunk-size 2M --vfs-read-chunk-size-limit 10M --vfs-cache-max-age 168h0m0s --vfs-cache-max-size ${CACHEMAXSIZE} --cache-dir=/storage/cache --cache-chunk-path /storage/cache-backend/ --cache-chunk-clean-interval 10m0s --log-file /sdcard/dns.log --allow-other --gid 1015 --daemon
+                $MODDIR/rclone mount ${line}: ${CLOUDROOTMOUNTPOINT}/${line} --config ${CONFIGFILE} --max-read-ahead ${READAHEAD} --buffer-size ${BUFFERSIZE} --dir-cache-time ${DIRCACHETIME} --poll-interval 5m --attr-timeout ${DIRCACHETIME} --vfs-cache-mode writes --vfs-read-chunk-size 2M --vfs-read-chunk-size-limit 10M --vfs-cache-max-age 168h0m0s --vfs-cache-max-size ${CACHEMAXSIZE} --cache-dir=/storage/cache --cache-chunk-path /storage/cache-backend/ --cache-chunk-clean-interval 10m0s --log-file /sdcard/dns.log --allow-other --gid 1015 --daemon
                 sleep 5
         done
 
