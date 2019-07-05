a033c13a824bbce5abc67bd37d740123d5ddb605
geofferey <geofferey.eakins@gmail.com>
2019-07-05 07:19:09 +0000

    Improve rclone-wrapper.sh

--- binary/rclone-wrapper.sh
+++ binary/rclone-wrapper.sh
@@ -2,13 +2,12 @@
 
 MODDIR=${0%/*}
 
-#. $MODDIR/module.prop >> /dev/null 2>&1
-
 IMGDIR=/sbin/.core/img
 id=com.piyushgarg.rclone
 
 USER_CONFDIR=/sdcard/.rclone
 CLOUDROOTMOUNTPOINT=/mnt/cloud
+SCRIPTPID=$$
 
 if [ -d ${IMGDIR}/${id} ]; then
 
@@ -22,30 +21,31 @@ fi
 
 disable () {
     
-    touch ${USER_CONFDIR}/.$*.disable
+    echo "disabling remote ${2}"
+    touch $USER_CONFDIR/.${2}.disable
 }
 
 unmount () {
     
+    kill $(pgrep -f rclone| grep -v $SCRIPTPID) >> /dev/null 2>&1
+    sleep 3
     umount -f ${CLOUDROOTMOUNTPOINT}/* >> /dev/null 2>&1
-    kill -9 $(pgrep -x rclone)  >> /dev/null 2>&1
-    
+    sleep 3
     rm -r ${CLOUDROOTMOUNTPOINT} >> /dev/null 2>&1
+
 }
 
 
 remount () { 
 
-    umount -f ${CLOUDROOTMOUNTPOINT}/* >> /dev/null 2>&1
-    sleep 1
+    unmount
     ${HOME}/service.sh
 
 }
 
 if [[ ${1} = disable ]]; then
 
-    echo "disabling remote ${2}"
-    touch $USER_CONFDIR/.${2}.disable
+    disable
 
 elif [[ ${1} = remount ]]; then
 
